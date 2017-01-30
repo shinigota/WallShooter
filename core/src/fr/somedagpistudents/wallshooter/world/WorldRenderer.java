2774428d9fa52b868012a6263af1f3fa0a3d088e
shinigota <ben.barbe10@gmail.com>
2017-01-30 16:26:50 +0100

    cleaning WorldRenderer

--- core/src/fr/somedagpistudents/wallshooter/world/WorldRenderer.java
+++ core/src/fr/somedagpistudents/wallshooter/world/WorldRenderer.java
@@ -16,19 +16,17 @@ public class WorldRenderer {
     public WorldRenderer(World world) {
         this.world = world;
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
     }
 
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
+
         batch.end();
     }
 
     public void dispose() {
         batch.dispose();
-        img.dispose();
     }
 }
