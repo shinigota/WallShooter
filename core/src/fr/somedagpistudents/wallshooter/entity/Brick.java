df030ad3f434d6a71fbbcb06063874950e1c0ccc
Bonnan Samuel <samuel.bonnan@etu.u-bordeaux.fr>
2017-01-30 16:55:22 +0100

    Modif Brick Width Height Const

--- core/src/fr/somedagpistudents/wallshooter/entity/Brick.java
+++ core/src/fr/somedagpistudents/wallshooter/entity/Brick.java
@@ -3,9 +3,11 @@ package fr.somedagpistudents.wallshooter.entity;
 public class Brick extends Entity {
     public static final float WIDTH  = 2;
     public static final float HEIGHT = 2;
+    public static final float BRICK_WIDTH = 60;
+    public static final float BRICK_HEIGHT = 35;
 
     public Brick(float x, float y) {
-        super(x, y);
+        super(x, y, BRICK_WIDTH, BRICK_HEIGHT);
     }
 
     @Override
--- core/src/fr/somedagpistudents/wallshooter/world/World.java
+++ core/src/fr/somedagpistudents/wallshooter/world/World.java
@@ -5,11 +5,13 @@ import fr.somedagpistudents.wallshooter.entity.Player;
 
 public class World {
     Player p;
-    Brick b;
+    Brick b1;
+    Brick b2;
 
     public World() {
         this.p = new Player(-640,0,20,30);
-        this.b = new Brick(640,0);
+        this.b1 = new Brick(640,0);
+        this.b2 = new Brick(320,100);
     }
 
     public World render(){
