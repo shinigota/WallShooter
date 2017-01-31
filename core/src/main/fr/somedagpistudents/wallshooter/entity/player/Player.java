d28b51558d0c3a5b6476b0ab6c4c798036a6f248
Damien <damien33520@gmail.com>
2017-01-31 12:23:59 +0100

    add score

--- core/src/main/fr/somedagpistudents/wallshooter/entity/player/Player.java
+++ core/src/main/fr/somedagpistudents/wallshooter/entity/player/Player.java
@@ -4,12 +4,21 @@ import fr.somedagpistudents.wallshooter.entity.MovableEntity;
 import fr.somedagpistudents.wallshooter.entity.wall.Brick;
 import fr.somedagpistudents.wallshooter.entity.weapon.Weapon;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by djacques on 30/01/17.
  */
 public class Player extends MovableEntity {
     private int score = 0;
     private Weapon weapon;
+    private Timer time;
+
+    public int getScore() {
+        return score;
+    }
+
     protected boolean blockedFront = false;
 
 
@@ -22,6 +31,9 @@ public class Player extends MovableEntity {
 
     @Override
     public void update() {
+        if(time == null){
+            createTimer();
+        }
         if(!this.blockedFront) {
             this.x += getXSpeed();
         }else{
@@ -43,5 +55,16 @@ public class Player extends MovableEntity {
 
     public Weapon getWeapon() {
         return weapon;
+    }
+
+    public void createTimer(){
+        time = new Timer("AddScore");
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                createTimer();
+                Player.this.score++;
+            }
+        },1000);
     }
 }
