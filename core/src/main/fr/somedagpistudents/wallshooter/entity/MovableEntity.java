24b64ae5e8dcc9960094a60a014763568c0eaee0
shinigota <ben.barbe10@gmail.com>
2017-01-31 14:40:31 +0100

    Fixed some entities constructor and updated Weapon / Bullet

--- core/src/main/fr/somedagpistudents/wallshooter/entity/MovableEntity.java
+++ core/src/main/fr/somedagpistudents/wallshooter/entity/MovableEntity.java
@@ -4,12 +4,15 @@ public abstract class MovableEntity extends Entity {
     protected float xSpeed;
     protected float ySpeed;
 
-    public MovableEntity(float x, float y, float width, float height) {
+    public MovableEntity(float x, float y, float width, float height, float xSpeed, float ySpeed) {
         super(x, y, width, height);
+
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
     }
 
-    public MovableEntity(float size, float width, float height) {
-        super(size, width, height);
+    public MovableEntity(float x, float y, float size, float xSpeed, float ySpeed) {
+        this(x, y, size, size, xSpeed, ySpeed);
     }
 
     public float getXSpeed() {
