239f6129b242a28471b8ed087d6d5bf34007b52b
DopaSensei <pierreyves.lgn@outlook.fr>
2017-01-31 14:49:21 +0100

    set dummy test on wallTest

--- core/src/test/fr/somedagpistudents/wallshooter/entity/wall/WallTest.java
+++ core/src/test/fr/somedagpistudents/wallshooter/entity/wall/WallTest.java
@@ -2,13 +2,15 @@ package fr.somedagpistudents.wallshooter.entity.wall;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by sbonnan on 31/01/17.
  */
 public class WallTest {
 
     @Test
-    public void () throws Exception {
-
+    public void DummyWallTest() throws Exception {
+        assertEquals(1,1);
     }
 }
