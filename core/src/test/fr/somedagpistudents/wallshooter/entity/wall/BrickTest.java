bc49886a3941333cf63416b6c6a4a559fe4feb6f
Bonnan Samuel <samuel.bonnan@etu.u-bordeaux.fr>
2017-01-31 12:23:54 +0100

    Creation Test Brick

---
+++ core/src/test/fr/somedagpistudents/wallshooter/entity/wall/BrickTest.java
@@ -0,0 +1,14 @@
+package fr.somedagpistudents.wallshooter.entity.wall;
+
+import org.junit.Test;
+
+/**
+ * Created by sbonnan on 31/01/17.
+ */
+public class BrickTest {
+
+    @Test
+    public void createBrickTest() throws Exception {
+
+    }
+}
