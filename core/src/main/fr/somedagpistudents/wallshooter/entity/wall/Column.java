1275e19f90dfefecadea22941153d01f6aff598c
Bonnan Samuel <samuel.bonnan@etu.u-bordeaux.fr>
2017-02-01 09:39:04 +0100

    Modif Column Wall

--- core/src/main/fr/somedagpistudents/wallshooter/entity/wall/Column.java
+++ core/src/main/fr/somedagpistudents/wallshooter/entity/wall/Column.java
@@ -1,21 +1,41 @@
 package fr.somedagpistudents.wallshooter.entity.wall;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.badlogic.gdx.math.MathUtils.random;
 
 /**
  * Created by sbonnan on 30/01/17.
  */
 public class Column {
+    private static final int POSITION_X = 640;
+    public final static int MAX_NB_BRICK = 12;
     private ArrayList<Brick> bricks = new ArrayList<Brick>();
 
-
-    public Column(int x, int[] pos) {
+    public Column() {
+        Set<Integer> pos = generatePosition();
         for(int position: pos ) {
-            this.bricks.add(calculPosBrick(x, position));
+            this.bricks.add(generateBrick(POSITION_X, position));
         }
     }
 
-    private Brick calculPosBrick(int x,int position){
+    public List<Brick> getBricks(){
+        return bricks;
+    }
+
+    private Set<Integer> generatePosition() {
+        HashSet<Integer> pos = new HashSet<Integer>();
+
+        for(int i=0;i<=MAX_NB_BRICK;i++){
+            pos.add(random(0,MAX_NB_BRICK));
+        }
+        return pos;
+    }
+
+    private Brick generateBrick(int x, int position){
         return new Brick(x, (position*60)-360);
     }
 
