3f94f7e94cea443caba60a22036ab966ca0de6e9
Franck Jude <franck.jude@free.fr>
2017-02-03 10:52:05 +0100

    bonuses raw

---
+++ core/src/main/fr/somedagpistudents/wallshooter/entity/bonus/Bonus.java
@@ -0,0 +1,11 @@
+package fr.somedagpistudents.wallshooter.entity.bonus;
+
+import fr.somedagpistudents.wallshooter.entity.wall.Brick;
+
+/**
+ * Created by fjude001 on 02/02/17.
+ */
+public class Bonus extends Brick {
+    
+    
+}
---
+++ core/src/main/fr/somedagpistudents/wallshooter/entity/bonus/BonusType.java
@@ -0,0 +1,7 @@
+package fr.somedagpistudents.wallshooter.entity.bonus;
+
+/**
+ * Created by fjude001 on 03/02/17.
+ */
+public class BonusType {
+}
